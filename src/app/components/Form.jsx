abc55cfcb08f4e2b7bf522517ec5a8cfd67b5dd3
MVCorreas <correasmv@gmail.com>
2025-09-02 10:36:44 -0300

    feat: added components inside form

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,3 +1,16 @@
 export default function Form() {
-  return <div className="add-form">Form</div>;
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {[...Array(10)].map((_, i) => (
+          <option key={i + 1} value={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </div>
+  );
 }
