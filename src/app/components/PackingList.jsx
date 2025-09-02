c0037b96e7dfc7f5fe4b09f9df0d5040e1ad4011
MVCorreas <correasmv@gmail.com>
2025-09-02 13:30:53 -0300

    style: aligned select and button

--- src/app/components/PackingList.jsx
+++ src/app/components/PackingList.jsx
@@ -1,3 +1,20 @@
 export default function PackingList() {
-  return <div className="list">Packing List</div>;
+  return (
+    <div className="list">
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+      <div className="actions">
+        <select>
+          <option value="SORT BY INPUT ORDER">SORT BY INPUT ORDER</option>
+          <option value="SORT BY ALPHABETICAL ORDER">
+            SORT BY ALPHABETICAL ORDER
+          </option>
+        </select>
+        <button className="actions">CLEAR LIST</button>
+      </div>
+    </div>
+  );
 }
