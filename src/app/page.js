bf0f7ff075a65e635e8fe3c88a3e418239ac17bb
MVCorreas <correasmv@gmail.com>
2025-09-02 10:27:23 -0300

    feat: added Stats component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import Stats from "./components/Stats";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
      <Header />
-     
+     <Stats />
     </div>
   );
 }
