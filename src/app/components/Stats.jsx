3e3a5f188df3b449d9675d244e26fb2b151e3eb2
MVCorreas <correasmv@gmail.com>
2025-09-02 10:30:39 -0300

    feat: added Form, Packing List and Stats basic sections and components

--- src/app/components/Stats.jsx
+++ src/app/components/Stats.jsx
@@ -1,10 +1,7 @@
 export default function Stats() {
   return (
     <div className="stats">
-      <h2>Travel Statistics</h2>
-      <p>Total Trips: 10</p>
-      <p>Favorite Destination: Paris</p>
-      <p>Most Visited Country: France</p>
+     Stats
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import Stats from "./components/Stats";
+import Form from "./components/Form";
+import PackingList from "./components/PackingList";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
      <Header />
+     <Form />
+     <PackingList />
      <Stats />
     </div>
   );
