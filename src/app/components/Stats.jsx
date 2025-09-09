85d480cea1647919aa436462cb97e95c63ea2db9
MVCorreas <correasmv@gmail.com>
2025-09-09 18:17:51 -0300

    feat: return a different message if there are no items

--- src/app/components/Stats.jsx
+++ src/app/components/Stats.jsx
@@ -1,8 +1,14 @@
 export default function Stats({ items }) {
   const totalItems = items.length;
   const packedItems = items.filter((item) => item.packed).length;
-  const percentage =
-    totalItems === 0 ? 0 : Math.round((packedItems / totalItems) * 100);
+  const percentage = Math.round((packedItems / totalItems) * 100);
+
+  if (!totalItems)
+    return (
+      <footer className="stats">
+        <em>Start adding some items 📝</em>
+      </footer>
+    );
 
   return (
     <footer className="stats">
